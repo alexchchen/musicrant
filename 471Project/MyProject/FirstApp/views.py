537ff4a5949ec45c4dff3ef4210bc2dc00c86d01
Alex Chen <83100991+alexchchen@users.noreply.github.com>
2023-11-27 21:55:44 -0700

    Link artist view with database
    Also updated related names for many models

--- 471Project/MyProject/FirstApp/views.py
+++ 471Project/MyProject/FirstApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from django.db.models import Avg, Value, ExpressionWrapper
+from django.db.models import Avg
 from .models import *
 
 def FirstView(request):
@@ -24,57 +24,100 @@ def main(request):
     template = loader.get_template('main.html')
     return HttpResponse(template.render())
 
+
 def homePage(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
 
+
 def topArtistsPage(request):
     artists = Artist.objects.annotate(
         overall_score = Avg(
-            (F('song__song_rating__originality_score') +
-             F('song__song_rating__lyric_score') +
-             F('song__song_rating__vibe_score') +
-             F('song__song_rating__instrumental_score')) / 4 
+            (F('songs__ratings__originality_score') +
+             F('songs__ratings__lyric_score') +
+             F('songs__ratings__vibe_score') +
+             F('songs__ratings__instrumental_score')) / 4 
         )
     ).order_by('-overall_score')[:10]
-    
+
     template = loader.get_template('topArtists.html')
-    
     context = {
         'artists': artists
     }
-    
     return HttpResponse(template.render(context, request))
 
-def artistPage(request):
+
+def artistPage(request, artist_id):
+    artist = Artist.objects.filter(artist_id=artist_id).annotate(
+        overall_score = Avg(
+            (F('songs__ratings__originality_score') +
+             F('songs__ratings__lyric_score') +
+             F('songs__ratings__vibe_score') +
+             F('songs__ratings__instrumental_score')) / 4 
+        )
+    )[0]
+    
+    songs = Song.objects.filter(artist_id=artist_id).annotate(
+        overall_score = Avg(
+            (F('ratings__originality_score') +
+             F('ratings__lyric_score') +
+             F('ratings__vibe_score') +
+             F('ratings__instrumental_score')) / 4 
+        )
+    )
+    
+    albums = Album.objects.filter(artist_id=artist_id).annotate(
+        overall_score = Avg(
+            (F('ratings__originality_score') +
+             F('ratings__lyric_score') +
+             F('ratings__vibe_score') +
+             F('ratings__instrumental_score') +
+             F('ratings__album_flow_score')) / 5 
+        )
+    )
+    
     template = loader.get_template('artist.html')
-    return HttpResponse(template.render())
+    context = {
+        'artist': artist,
+        'songs': songs,
+        'albums': albums
+    }
+    return HttpResponse(template.render(context, request))
+
 
 def producerPage(request):
     template = loader.get_template('producer.html')
     return HttpResponse(template.render())
 
+
 def giveRating(request):
     template = loader.get_template('giveRating.html')
+
+
 def topAlbumsPage(request):
     template = loader.get_template('topAlbums.html')
     return HttpResponse(template.render())
+
 
 def topSongsPage(request):
     template = loader.get_template('topSongs.html')
     return HttpResponse(template.render())
 
+
 def loginPage(request):
     template = loader.get_template('login.html')
     return HttpResponse(template.render())
+
 
 def userPage(request):
     template = loader.get_template('user.html')
     return HttpResponse(template.render())
 
+
 def singleSongPage(request):
     template = loader.get_template('singleSong.html')
     return HttpResponse(template.render())
+
 
 def singleAlbumPage(request):
     template = loader.get_template('singleAlbum.html')
